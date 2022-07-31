d50c18732f317dad0bc3859386e14e39e74df67e
kojima yosuke <yabbee0828@gmail.com>
2022-08-01 02:13:50 +0900

    Set up redux dev tool

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,20 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { compose, applyMiddleware } from 'redux';
-// import { composeWithDevTools } from 'remote-redux-devtools';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'remote-redux-devtools';
 import reduxThunk from 'redux-thunk';
 import rootReducer from './reducers/root';
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = configureStore(
-  {
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-        serializableCheck: false,
-      }),
-  },
-  composeEnhancer(applyMiddleware(reduxThunk))
+const composeEnhancers = composeWithDevTools({ realtime: true, port: 8000 });
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(reduxThunk)
+    // other store enhancers if any
+  )
 );
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const store = configureStore(
+//   {
+//     reducer: rootReducer,
+//     middleware: (getDefaultMiddleware) =>
+//       getDefaultMiddleware({
+//         serializableCheck: false,
+//       }),
+//   },
+//   composeEnhancers(applyMiddleware(reduxThunk))
+// );
 
 export default store;
