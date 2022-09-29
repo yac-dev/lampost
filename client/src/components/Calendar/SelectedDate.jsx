4cf8e3be45a579b1dc0e5d9cc729efbd1ad259dd
kojima yosuke <yabbee0828@gmail.com>
2022-09-29 22:04:57 +0900

    Added calendar at schedule component

---
+++ client/src/components/Calendar/SelectedDate.jsx
@@ -0,0 +1,13 @@
+// main libraries
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SelectedDate = () => {
+  return (
+    <View>
+      <Text>This is the selected Date component</Text>
+    </View>
+  );
+};
+
+export default SelectedDate;
